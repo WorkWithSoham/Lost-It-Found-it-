69a5923fbbb216b6b0a872c7b51b9e90f9b74b45
WorkWithSoham <sohamtembe98@gmail.com>
2023-04-02 18:28:56 -0500

    Landing page and login page designed.

--- app/src/main/java/com/example/lostitfoundit/LoginPage.java
+++ app/src/main/java/com/example/lostitfoundit/LoginPage.java
@@ -2,14 +2,13 @@ package com.example.lostitfoundit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_page);
     }
 }
