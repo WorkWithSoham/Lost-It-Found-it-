c1f43fad896deaf86ab8548b327a9f8269d31ce6
WorkWithSoham <sohamtembe98@gmail.com>
2023-04-02 20:49:56 -0500

    Signup Page integrated

--- app/src/main/java/com/example/lostitfoundit/LoginPage.java
+++ app/src/main/java/com/example/lostitfoundit/LoginPage.java
@@ -2,13 +2,31 @@ package com.example.lostitfoundit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
+    TextView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        backBtn = (TextView) findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLandingPage();
+            }
+        });
+    }
+
+    public void goToLandingPage() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/lostitfoundit/SignUpPage.java
+++ app/src/main/java/com/example/lostitfoundit/SignUpPage.java
@@ -2,13 +2,30 @@ package com.example.lostitfoundit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUpPage extends AppCompatActivity {
+    TextView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+
+        backBtn = (TextView) findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLandingPage();
+            }
+        });
+    }
+
+    public void goToLandingPage() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
